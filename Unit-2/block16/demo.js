0d12497a5c088ccbe5e7af167e6252fd24d890e0
Ryan <ryfly2016@gmail.com>
2025-03-25 10:57:57 -0600

    add and complete block 16

---
+++ Unit-2/block16/demo.js
@@ -0,0 +1,24 @@
+const numArr = [1, 5, 3, 6, 2, 1, 8, 7];
+
+// for (i in numArr) {
+//   // could also do: for (let i = 0; i < numArr.length; i++) {}
+//   console.log(`index ${i} contains ${numArr[i]}`);
+// }
+
+// to print each index
+// numArr.forEach((index) => {
+//   console.log(index);
+// });
+
+// to print each element
+// numArr.forEach((value, index) => {
+//   console.log(`${index}: ${value}`);
+// });
+
+// const nameArr = ["carl", "jacob", "nancy", "sue"];
+// const dogFound = nameArr.find((element) => {
+//   if (element === "nancy") {
+//     return element;
+//   }
+// });
+// console.log(dogFound);
