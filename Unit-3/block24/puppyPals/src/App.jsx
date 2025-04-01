03e539272d6a67a8537f210b09eb992c558a9fd1
Ryan <ryfly2016@gmail.com>
2025-04-01 10:30:05 -0600

    create data.js and map it in app

--- Unit-3/block24/puppyPals/src/App.jsx
+++ Unit-3/block24/puppyPals/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { puppyList } from "./data";
+import { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [puppies, setPuppies] = useState(puppyList);
+  return (
+    <div>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
