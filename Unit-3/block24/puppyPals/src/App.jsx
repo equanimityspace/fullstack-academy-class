e3f9b25e7b485bb6ec4f11419a3908435b39ee29
Ryan <ryfly2016@gmail.com>
2025-04-01 10:51:50 -0600

    add featured puppy functionality

--- Unit-3/block24/puppyPals/src/App.jsx
+++ Unit-3/block24/puppyPals/src/App.jsx
@@ -3,12 +3,39 @@ import { puppyList } from "./data";
 import { useState } from "react";
 
 function App() {
+  // for full array of puppies
   const [puppies, setPuppies] = useState(puppyList);
+
+  // for featured puppy, useState(null) so that it can be conditionally rendered
+  const [featPupID, setFeatPupID] = useState(null);
+
+  // read ID and get info on featured puppy
+  const featuredPup = puppies.find((pup) => pup.id === featPupID);
+  console.log(featuredPup);
+
   return (
     <div>
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupID(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      {featPupID && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
